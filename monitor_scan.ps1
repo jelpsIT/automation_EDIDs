2eef4b458565024f64ab49379d7813c6bffcf124
jelpsIT <106605874+jelpsIT@users.noreply.github.com>
2023-03-09 12:19:07 +0000

    Update monitor_scan.ps1

--- monitor_scan.ps1
+++ monitor_scan.ps1
@@ -1,11 +1,11 @@
 $url = 'https://github.com/jelpsIT/automation_EDIDs/raw/main/DumpEDID.exe'
-$outpath = "$env:USERPROFILE/Downloads/DumpEDID.exe"
+$outpath = "C:/temp/DumpEDID.exe"
 Invoke-WebRequest -Uri $url -OutFile $outpath
  
 $wc = New-Object System.Net.WebClient
 $wc.DownloadFile($url, $outpath)
 
 Start-Sleep -s 2
-Set-Location -Path $env:USERPROFILE/Downloads
+Set-Location -Path C:\temp
 
 Start-Process .\DumpEDID.exe -NoNewWindow
